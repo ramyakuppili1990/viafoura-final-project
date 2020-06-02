0bec6f00bad8d860ad8b6a2800c051e19ec05dd8
ramya kuppili <ramyakuppili@gmail.com>
2020-06-02 16:32:36 -0400

    Viafoura Final Project

---
+++ AnagramChecker/src/main/java/com/coding/anagrams/AnagramCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.coding.anagrams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramCheckerApplication.class, args);
+	}
+
+}
---
+++ AnagramChecker/src/test/java/com/coding/anagrams/AnagramCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.anagrams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
